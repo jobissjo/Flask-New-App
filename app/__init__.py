2adc540ef1a33be7e2512d42a0e60fa76753dc89
jo <15617279-jobiivar@users.noreply.replit.com>
2024-11-12 15:53:46 +0000

    Init bcrypt corrected

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
-bcrypt = Bcrypt
+bcrypt = Bcrypt()
 
 def create_app():
   
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from app import create_app
 
 app = create_app()
-
+print(app)
 @app.route('/')
 def index():
     return 'Hello from Flask!'
